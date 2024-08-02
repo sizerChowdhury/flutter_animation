df8f31034ea17d8a92034956b83f20880de9158f
sizer <shahin.reza@brainstation-23.com>
2024-08-02 19:22:21 +0600

    refactor: hero

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -2,9 +2,9 @@ import 'package:animation/models/trip_model.dart';
 import 'package:flutter/material.dart';
 
 class DetailsPage extends StatelessWidget {
-
   final TripModel trip;
-  const DetailsPage({required this.trip});
+
+  const DetailsPage({super.key, required this.trip});
 
   @override
   Widget build(BuildContext context) {
@@ -13,50 +13,43 @@ class DetailsPage extends StatelessWidget {
           backgroundColor: Colors.transparent,
         ),
         extendBodyBehindAppBar: true,
-        body: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              ClipRRect(
-                  child: Hero(
-                    tag: trip.image,
-                    child: Image.asset(
-                      'images/${trip.image}',
-                      height: 360,
-                      fit: BoxFit.cover,
-                      alignment: Alignment.topCenter,
-                    ),
-                  )
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            ClipRRect(
+                child: Hero(
+              tag: trip.image,
+              child: Image.asset(
+                'images/${trip.image}',
+                height: 360,
+                fit: BoxFit.cover,
+                alignment: Alignment.topCenter,
               ),
-              const SizedBox(height: 30),
-              ListTile(
-                  title: Text(
-                      trip.title,
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Colors.grey[800]
-                      )
-                  ),
-                  subtitle: Text(
-                      '${trip.night} night stay for only \$${trip.price}',
-                      style: TextStyle(letterSpacing: 1)
-                  ),
-                  //trailing: Heart()
+            )),
+            const SizedBox(height: 30),
+            ListTile(
+              title: Text(trip.title,
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.grey[800])),
+              subtitle: Text(
+                  '${trip.night} night stay for only \$${trip.price}',
+                  style: const TextStyle(letterSpacing: 1)),
+              trailing: IconButton(
+                icon: Icon(
+                  Icons.favorite,
+                  color: Colors.grey[400],
+                  size: 30,
+                ),
+                onPressed: () {},
               ),
-              Padding(
-                  padding: const EdgeInsets.all(18),
-                  child: Text(
-                      'Details about the product or hotel',
-                      style: TextStyle(
-                          color: Colors.grey[600],
-                          height: 1.4
-                      )
-                  )
-              ),
-            ],
-          ),
-        )
-    );
+            ),
+            Padding(
+                padding: const EdgeInsets.all(18),
+                child: Text('Details about the product or hotel',
+                    style: TextStyle(color: Colors.grey[600], height: 1.4))),
+          ],
+        ));
   }
 }
